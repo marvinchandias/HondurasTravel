a8b9f501a4113200903a772124630c1ceff37bb6
AdesAdes <mrnrades@gmail.com>
2015-10-25 01:33:07 -0600

    jquery dinamico

--- JS/js.js
+++ JS/js.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
-   
-   
    $("#btnBoletines").click(function(event) {
-        event.preventDefault(); 
-         
+        event.preventDefault();   
           $("iframe").remove();
-          $("#ContenedorInformacion").html("<iframe src='Pages/pruebasBoletines/index2.html' scrolling='no'></iframe>");
-           
+          $("#ContenedorInformacion").html("<iframe src='Pages/pruebasBoletines/index2.html' scrolling='no'></iframe>");           
     });
-  
     $("#btnPlayas").click(function(event) {
         event.preventDefault(); 
-         
           $("iframe").remove();
-          
-           
     });
-
 });
